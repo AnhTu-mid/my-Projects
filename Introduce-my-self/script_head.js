c436b95eb493c69f37a6fa2888b43552880366c4
AnhTu-MidProgramer <hn17111509@gmail.com>
2024-08-27 16:08:10 +0700

    Update script_head.js

--- Introduce-my-self/script_head.js
+++ Introduce-my-self/script_head.js
@@ -1,4 +1,4 @@
-//Head Script//
+//Head & Foot Script//
 
 var objectives = ['Ab-me', 'My-prj']
 
@@ -7,11 +7,21 @@ const info2_my_prj = document.querySelector('.info2')
 
 for(let i =0; i<objectives.length; i++){
 	const obj = document.getElementById(objectives[i])
+	const obj2 = document.getElementById(`${objectives[i]}_foot`)
 
 	obj.addEventListener('click', function(){
 		if(i == 0 && objectives[i] == 'Ab-me'){
 			info1_ab_me.scrollIntoView({ behavior: 'smooth' })
 		}else if(i == 1 && objectives[i] == 'My-prj'){
+			info2_my_prj.scrollIntoView({ behavior: 'smooth' })
+		}
+	})
+
+	console.log(`${objectives[i]}_foot`)
+	obj2.addEventListener('click', function(){
+		if(i == 0 && objectives[i] + "_foot" == 'Ab-me_foot'){
+			info1_ab_me.scrollIntoView({ behavior: 'smooth' })
+		}else if(i == 1 && objectives[i] + "_foot" == 'My-prj_foot'){
 			info2_my_prj.scrollIntoView({ behavior: 'smooth' })
 		}
 	})
